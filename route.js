0efccbe56e878009edcc835826ec554f16e3b527
Anaglebia <bia22.sp@icloud.com>
2024-02-27 21:36:58 -0300

    Cria~ção inicail

--- route.js
+++ route.js
@@ -1,1 +1,10 @@
-// Vamos incluir nossas rotas aqui
+const { getAll, create } = require('./controller')
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/cards', getAll)
+
+router.post('/cards', create)
+
+module.exports = router
